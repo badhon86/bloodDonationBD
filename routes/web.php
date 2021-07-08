95eb4c1d9e2cc8e0f5cfb5b6588917c54e0f5394
badhon86 <badhon35-2092@diu.edu.bd>
2021-07-08 22:36:46 +0600

    donar page

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome2');
 });
 
-
+//Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 //auth route for both
 Route::group(['middleware' => ['auth']], function() {
