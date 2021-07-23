6aebb2e6c9ea1ef5deb26aec07933404e119c4ab
badhon86 <badhon35-2092@diu.edu.bd>
2021-07-23 22:59:41 +0600

    donarlist.blade

---
+++ resources/views/donarlist.blade.php
@@ -0,0 +1,102 @@
+@extends('layouts.welcome2nav')
+@section('main')
+<br>
+<br>
+
+<!-- ======= donar Section ======= -->
+<section id="pricing" class="pricing">
+    <div class="container" data-aos="fade-up">
+
+      <div class="section-title">
+        <h2>রক্তদাতা</h2>
+        <h3>রক্তদাতাদের <span>তালিকা</span></h3>
+
+      </div>
+
+      <div class="row">
+
+
+        <div class="col-lg-3 col-md-6 mt-4 mt-md-0" data-aos="fade-up" data-aos-delay="200">
+          <div class="box featured">
+            <h3>বাঁধন সরকার </h3>
+            <h4><sup>গ্রুপ</sup>AB<span> (+ve)</span></h4>
+            <ul>
+              <li><i class="icofont-ui-call"></i>+880 1797589372</li>
+              <li><i class="icofont-address-book"></i>Salimullah RD, Mohammadpur, Dhaka-1207</li>
+              <li><i class="icofont-users-alt-4"></i>Male</li>
+              <li><i class="icofont-clock-time"></i>Last Donate: 06/06/2021</li>
+            </ul>
+            <div class="social">
+              <a href=""><i class="icofont-twitter"></i></a>
+              <a href=""><i class="icofont-facebook"></i></a>
+              <a href=""><i class="icofont-instagram"></i></a>
+              <a href=""><i class="icofont-linkedin"></i></a>
+            </div>
+         </div>
+         <br>
+        </div>
+        <div class="col-lg-3 col-md-6 mt-4 mt-md-0" data-aos="fade-up" data-aos-delay="200">
+          <div class="box featured">
+            <h3>বাঁধন সরকার </h3>
+            <h4><sup>গ্রুপ</sup>AB<span> (+ve)</span></h4>
+            <ul>
+              <li><i class="icofont-ui-call"></i>+880 1797589372</li>
+              <li><i class="icofont-address-book"></i>Salimullah RD, Mohammadpur, Dhaka-1207</li>
+              <li><i class="icofont-users-alt-4"></i>Male</li>
+              <li><i class="icofont-clock-time"></i>Last Donate: 06/06/2021</li>
+            </ul>
+            <div class="social">
+              <a href=""><i class="icofont-twitter"></i></a>
+              <a href=""><i class="icofont-facebook"></i></a>
+              <a href=""><i class="icofont-instagram"></i></a>
+              <a href=""><i class="icofont-linkedin"></i></a>
+            </div>
+         </div>
+         <br>
+        </div>
+        <div class="col-lg-3 col-md-6 mt-4 mt-md-0" data-aos="fade-up" data-aos-delay="200">
+          <div class="box featured">
+            <h3>বাঁধন সরকার </h3>
+            <h4><sup>গ্রুপ</sup>AB<span> (+ve)</span></h4>
+            <ul>
+              <li><i class="icofont-ui-call"></i>+880 1797589372</li>
+              <li><i class="icofont-address-book"></i>Salimullah RD, Mohammadpur, Dhaka-1207</li>
+              <li><i class="icofont-users-alt-4"></i>Male</li>
+              <li><i class="icofont-clock-time"></i>Last Donate: 06/06/2021</li>
+            </ul>
+            <div class="social">
+              <a href=""><i class="icofont-twitter"></i></a>
+              <a href=""><i class="icofont-facebook"></i></a>
+              <a href=""><i class="icofont-instagram"></i></a>
+              <a href=""><i class="icofont-linkedin"></i></a>
+            </div>
+         </div>
+         <br>
+        </div>
+        <div class="col-lg-3 col-md-6 mt-4 mt-md-0" data-aos="fade-up" data-aos-delay="200">
+          <div class="box featured">
+            <h3>বাঁধন সরকার </h3>
+            <h4><sup>গ্রুপ</sup>AB<span> (+ve)</span></h4>
+            <ul>
+              <li><i class="icofont-ui-call"></i>+880 1797589372</li>
+              <li><i class="icofont-address-book"></i>Salimullah RD, Mohammadpur, Dhaka-1207</li>
+              <li><i class="icofont-users-alt-4"></i>Male</li>
+              <li><i class="icofont-clock-time"></i>Last Donate: 06/06/2021</li>
+            </ul>
+            <div class="social">
+              <a href=""><i class="icofont-twitter"></i></a>
+              <a href=""><i class="icofont-facebook"></i></a>
+              <a href=""><i class="icofont-instagram"></i></a>
+              <a href=""><i class="icofont-linkedin"></i></a>
+            </div>
+         </div>
+         <br>
+        </div>
+
+
+
+      </div>
+
+    </div>
+  </section><!-- End donar Section -->
+@endsection
